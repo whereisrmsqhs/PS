6f8a92ff9763c8341f6098f0b067992f17d84e58
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-08-11 00:32:48 +0900

    백준 15650

---
+++ 백준15650.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define MAX 9
+using namespace std;
+
+int N, M;
+int arr[MAX];
+
+
+void dfs(int num, int k){
+  if(k == M){
+    for(int i = 0; i < M; i++){
+      cout << arr[i] << " ";
+    }
+    cout << "\n";
+  }
+  else {
+    for(int i = num; i <= N; i++){ // for문을 건드려야 했음... 이전 문제와 비교했을 때
+      arr[k] = i;
+      dfs(i+1, k+1);
+    }
+  }
+}
+
+int main(){
+  cin >> N >> M;
+
+  dfs(1, 0);
+}
