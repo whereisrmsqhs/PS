974b8ea50a2fc0aa76e5963041a7113e0135a99d
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-10-27 22:37:18 +0900

    백준 2743

---
+++ problems/백준2743.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  string s;
+  cin >> s;
+
+  cout << s.length() << "\n";
+  return 0;
+}
