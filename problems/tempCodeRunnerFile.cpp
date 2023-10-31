fd589b377af0cb34653a637b77029f188f75ac40
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-10-31 10:30:45 +0900

    백준 17071

--- problems/tempCodeRunnerFile.cpp
+++ problems/tempCodeRunnerFile.cpp
@@ -1,1 +1,48 @@
-cout << "adad" << "\n";
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int visited[2][500001];
+int N, K;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> N >> K;
+  if(N==K){
+    cout << 0 << "\n";
+    return 0;
+  }
+
+  queue<int> q;
+  q.push(N);
+  visited[0][N] = 1;
+
+  int t = 1;
+  bool meet = false;
+  while(!q.empty()){
+    K += t;
+    if(visited[t%2][K]){
+      meet = true;
+      break;
+    }
+    int qsize = q.size();
+    for(int i = 0; i < qsize; i++){
+      int here = q.front(); q.pop();
+      for(int there : {here+1, here-1, here*2}){
+        if(there < 0 || there >= 500001 || visited[t%2][there]==1) continue;
+        if(there==K) { meet = true; break;}
+        visited[t%2][there] = 1;
+        q.push(there);
+      }
+      if(meet) break;
+    }
+    if(meet) break;
+    t++;
+  }
+  (meet) ? (cout << t) : (cout << -1);
+  cout << "\n";
+  return 0;
+}
--- problems/백준17071.cpp
+++ problems/백준17071.cpp
@@ -1,64 +1,9 @@
 #include<iostream>
 #include<queue>
-#include<vector>
 using namespace std;
 
+int visited[2][500001];
 int N, K;
-vector<bool> visited(500001);
-vector<int> road(500001);
-int to_move = 0;
-
-int bfs(){
-  queue<pair<int, int>> q;
-  q.push({K, 2}); q.push({N, 1}); // 1이면 수빈, 2면 동생 
-  road[N] = 0;  road[K] = 0;
-  visited[N] = true;
-  while(!q.empty()){
-
-    int idx = q.front().first;
-    int who = q.front().second;
-    // cout << "idx: " << idx << ", who: " << who << "\n";
-    q.pop();
-
-    if(who==1){ // 수빈인 경우 
-      if(idx+1 <= 500000 && visited[idx+1]==false){
-        if(road[idx+1]==road[idx]+1){
-          return road[idx+1];
-        }
-        q.push({idx+1, 1});
-        road[idx+1] = road[idx]+1;
-        visited[idx+1]=true;
-      }
-      if(idx-1 >= 0 && visited[idx-1]==false){
-        if(road[idx-1]==road[idx]+1){
-          return road[idx-1];
-        }
-        q.push({idx-1, 1});
-        road[idx-1] = road[idx]+1;
-        visited[idx-1] = true;
-      }
-      if(idx*2 <= 500000 && visited[idx*2]==false){
-        if(road[idx*2]==road[idx]+1){
-          return road[idx*2];
-        }
-        q.push({idx*2, 1});
-        road[idx*2] = road[idx]+1;
-        visited[idx*2] = true;
-      }
-    }
-
-    if(who==2){  // 동생인 경우
-      to_move++;
-      if(idx+to_move > 500000){
-        return -1;
-      }
-      q.push({idx+to_move, 2});
-      road[idx+to_move] = road[idx]+1;
-      visited[idx+to_move] = false;
-    }
-  }
-  return -1;
-}
 
 int main(){
   ios_base::sync_with_stdio(false);
@@ -66,22 +11,39 @@ int main(){
   cout.tie(NULL);
 
   cin >> N >> K;
-
   if(N==K){
     cout << 0 << "\n";
     return 0;
   }
 
-  fill(visited.begin(), visited.end(), false);
-  fill(road.begin(), road.end(), -100);
+  queue<int> q;
+  q.push(N);
+  visited[0][N] = 1;
 
-  int ans = bfs();
-  if(ans == -1){
-    cout << -1 << "\n";
+  int t = 1;
+  bool meet = false;
+  while(!q.empty()){
+    K += t;
+    if(K >= 500001) break;
+    if(visited[t%2][K]){
+      meet = true;
+      break;
+    }
+    int qsize = q.size();
+    for(int i = 0; i < qsize; i++){
+      int here = q.front(); q.pop();
+      for(int there : {here+1, here-1, here*2}){
+        if(there < 0 || there >= 500001 || visited[t%2][there]==1) continue;
+        if(there==K) { meet = true; break;}
+        visited[t%2][there] = 1;
+        q.push(there);
+      }
+      if(meet) break;
+    }
+    if(meet) break;
+    t++;
   }
-  else{
-    cout << ans << "\n";
-  }
+  (meet) ? (cout << t) : (cout << -1);
+  cout << "\n";
   return 0;
-
 }
